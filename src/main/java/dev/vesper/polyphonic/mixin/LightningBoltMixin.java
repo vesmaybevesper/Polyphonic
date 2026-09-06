6254e21815b71508022f2d0c23f2c53470719962
VesMaybeVesper <vesmaybevesper@gmail.com>
2026-09-06 18:51:55 -0400

    Update LightningBoltMixin.java

--- src/main/java/dev/vesper/polyphonic/mixin/LightningBoltMixin.java
+++ src/main/java/dev/vesper/polyphonic/mixin/LightningBoltMixin.java
@@ -19,7 +19,7 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 
-/* *
+/**
  * @author Vesper (VesMaybeVesper)
  */
 @Mixin(LightningBolt.class)
